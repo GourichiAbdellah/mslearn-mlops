1431b95b6b3e39c1027f338428e769e55415b7e6
GourichiAbdellah <abdegourichi@gmail.com>
2026-08-31 17:20:21 +0100

    change instance type

--- src/deploy_to_online_endpoint.py
+++ src/deploy_to_online_endpoint.py
@@ -72,7 +72,7 @@ def create_or_update_deployment(
         name=deployment_name,
         endpoint_name=endpoint_name,
         model=model,
-        instance_type="Standard_D32d_v5",
+        instance_type="Standard_A2_v2",
         instance_count=1,
         data_collector=get_data_collector(),
     )
